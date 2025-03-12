f1970fa3e464f614fdad7a79808002aeb0bb5447
Roberto Torres <kevinrobertotorresruiz@gmail.com>
2025-03-12 10:16:24 -0600

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://catania.com.mx', 'https://adminpanelcatania.netlify.app', 'http://localhost:5175'], // Añade 5175
+    'allowed_origins' => ['https://catania.com.mx', 'https://adminpanelcatania.netlify.app'], // Añade 5175
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
