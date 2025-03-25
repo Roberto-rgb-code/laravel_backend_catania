5fbba86851f8e5b7923d58a5ad08077ecceb889e
Roberto Torres <kevinrobertotorresruiz@gmail.com>
2025-03-25 13:24:24 -0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UniformeController;
+use App\Http\Controllers\UniformeFotoController;
 
 Route::middleware('api')->group(function () {
+    // Rutas para Uniformes
     Route::get('/uniformes', [UniformeController::class, 'index']);
     Route::get('/uniformes/{id}', [UniformeController::class, 'show']);
     Route::post('/uniformes', [UniformeController::class, 'store']);
     Route::put('/uniformes/{id}', [UniformeController::class, 'update']);
     Route::delete('/uniformes/{id}', [UniformeController::class, 'destroy']);
+
+    // Ruta para eliminar fotos individuales
+    Route::delete('/fotos/{id}', [UniformeFotoController::class, 'destroy'])->name('fotos.destroy');
 });
